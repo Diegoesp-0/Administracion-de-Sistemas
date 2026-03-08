4776def9201da2a2fa15e08b1bc2b1c0a65bb66d
Espiritu <diegoesp1011@gmail.com>
2026-03-07 17:27:37 -0700

    Update colores.sh

--- Funciones/Linux/colores.sh
+++ Funciones/Linux/colores.sh
@@ -4,21 +4,17 @@ verde='\033[0;32m'
 azul='\033[1;34m'
 cyan='\033[0;36m'
 nc='\033[0m'
-naranja='\033[38;5;214m' 
+naranja='\033[38;5;214m'
 
-# Funciones para imprimir mensajes segun que se ocupa
 print_error(){
     echo -e "${rojo}$1${nc}"
 }
-
 print_completado(){
     echo -e "${verde}$1${nc}"
 }
-
 print_info(){
     echo -e "${amarillo}$1${nc}"
 }
-
 print_titulo(){
     echo -e "\n${azul}==== $1 ====${nc}\n"
 }
