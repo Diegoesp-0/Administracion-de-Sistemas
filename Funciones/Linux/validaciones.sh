5f9dba02ba2ee58510812b8eb0bc540ec8e00aac
Espiritu <diegoesp1011@gmail.com>
2026-03-07 17:23:58 -0700

    Update validaciones.sh

--- Funciones/Linux/validaciones.sh
+++ Funciones/Linux/validaciones.sh
@@ -1,6 +1,6 @@
 # ---------- Cargar libreria compartida ----------
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/utils.sh"
+source "$SCRIPT_DIR/colores.sh"
 
 # Validaciones de IP
 validar_IP(){
